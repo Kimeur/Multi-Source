0f9c5bad42aaf1ccb69c5b3f1a7a4191aef4bc98
Gazouzi Marouane <gazouzimarouane@gmail.com>
2017-01-27 15:52:56 +0100

    Update, New database

--- src/javafxapplication2/FXMLDocumentController.java
+++ src/javafxapplication2/FXMLDocumentController.java
@@ -6,13 +6,17 @@
 package javafxapplication2;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -22,6 +26,8 @@ import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  *
@@ -84,7 +90,29 @@ public class FXMLDocumentController implements Initializable {
         }listcont.setItems(items);  
     }
     
+    @FXML
+    private void handleAboutAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("about.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = new Stage();
+           stage.initModality(Modality.APPLICATION_MODAL);
+           stage.setOpacity(1);
+           stage.setTitle("About the system");
+           stage.setScene(new Scene(root));
+           stage.showAndWait();
+           stage.setResizable(false);
+    }
+    @FXML 
+    
+    private javafx.scene.control.Button closeButton;
+
+    @FXML
+       private void closeButtonAction(){
    
+    Stage stage = (Stage) closeButton.getScene().getWindow();
+    
+    stage.close();
+}
     
 
     @Override
