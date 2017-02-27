3cd12943a8964e0158eb80ccc8119af9f4f9477a
MamadouBah1 <mad1korse@yahoo.fr>
2017-02-27 17:30:05 +0100

    Update Chercher.java

--- src/javafxapplication2/Chercher.java
+++ src/javafxapplication2/Chercher.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import static java.rmi.server.ObjID.read;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,17 +23,15 @@ public class Chercher {
      
     }
     
-    public ArrayList<String> strategie(ArrayList<String> maLis) throws FileNotFoundException, IOException{
+ public ArrayList<String> strategie(ArrayList<String> maLis) throws FileNotFoundException, IOException{
                                                 ArrayList<String> maListe = new ArrayList();
         
             try{
-                File file = new File ("/home/mamadou/workspace/Multi-Source-master/src/javafxapplication2/entree.txt");
+                File file = new File ("/home/mamadou/workspace/Multi-Source-master/src/javafxapplication2/imdb.txt");
                 FileReader fr = new FileReader (file);
                 BufferedReader br = new BufferedReader (fr);
-                
-                for(int i = 0; i<maLis.size();i++){
-
-                String cetteValeur = maLis.get(i);
+                                
+                String cetteValeur = maLis.get(0);
                     try{
                         String ligne="";                   
                         while(ligne!=null){                     
@@ -41,14 +40,15 @@ public class Chercher {
                                         System.out.println("le mot est trouvé: ");
                                                 System.out.println (ligne); 
                                                 maListe.add(ligne);
+                                                
                             }                                                                                    
                         }                                    
-                                         System.out.println ("none : ");
+                                        
                     } 
                         catch (IOException exception){
                                            System.out.println ("Erreur lors de la lecture : ");
                     }  
-                }
+                
             }
                 catch (FileNotFoundException e){
             System.out.println("le fichier n'a pas été trouvé.");             
